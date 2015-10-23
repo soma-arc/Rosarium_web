e6724abe457b24656b069f734d151c8014cc02c7
soma_arc <somaarcr@gmail.com>
2015-10-23 16:32:09 +0900

    changed defun to progn to remove  Warning: compilation failed, added c-quot, and exported to-complex

--- src/ps/common/complex.lisp
+++ src/ps/common/complex.lisp
@@ -10,87 +10,90 @@
   (:export :complex
            :c-sum
            :c-diff
-           :c-prod))
+           :c-prod
+           :c-quot
+           :to-complex))
 
 (in-package :rosarium_web.complex)
 
-(defun build-complex ()
-  (ps:ps
-    (defpsclass complex (&optional (re 0) (i 0))
-                (setq this.re re)
-                (setq this.i i))
-    (defps-static complex
-        +infinity+
-      (new (complex (@ -number +positive_infinity+)
-                    (@ -number +positive_infinity+))))
-    (defps-static complex
-        +zero+
-      (new (complex)))
-    (defps-static complex
-        +one+
-      (new (complex 1)))
-    (defps-static complex
-        +i+
-      (new (complex 0 1)))
-    (defps-static complex
-        +epsilon+ 0.000001)
+(progn
+  (setf (gethash #P"common/complex.js" *built-js-dic*)
+        (ps:ps
+          (defpsclass complex (&optional (re 0) (i 0))
+                      (setq this.re re)
+                      (setq this.i i))
+          (defps-static complex
+              +infinity+
+            (new (complex (@ -number +positive_infinity+)
+                          (@ -number +positive_infinity+))))
+          (defps-static complex
+              +zero+
+            (new (complex)))
+          (defps-static complex
+              +one+
+            (new (complex 1)))
+          (defps-static complex
+              +i+
+            (new (complex 0 1)))
+          (defps-static complex
+              +epsilon+ 0.000001)
 
-    (defpsmethod complex add (c)
-                 (new (complex (+ this.re c.re)
-                               (+ this.i c.i))))
-    (defpsmethod complex sub (c)
-                 (new (complex (- this.re c.re)
-                               (- this.i c.i))))
-    (defpsmethod complex mult (c)
-                 (new (complex (- (* this.re c.re)
-                                  (* this.i c.i))
-                               (+ (* this.re c.i)
-                                  (* this.i c.re)))))
-    (defpsmethod complex div (c)
-                 (let ((denom (+ (* c.re c.re)
-                                 (* c.i c.i))))
-                   (cond ((= denom 0)
-                          (@ complex +infinity+))
-                         ((= denom (@ -number +positive_infinity+))
-                          (@ complex +zero+))
-                         (t (new (complex (/ (+ (* this.re c.re)
-                                                (* this.i c.i))
-                                             denom)
-                                          (/ (- (* this.i c.re)
-                                                (* this.re c.i))
-                                             denom)))))))
-    (defps-static complex dist
-      (lambda (c1 c2)
-        (sqrt (+ (expt (- c1.re c2.re) 2)
-                 (expt (- c1.i c2.i) 2)))))
-    (defps-static complex sqrt
-      (lambda (c)
-        (let ((len (sqrt (+ (expt (- c1.re c2.re) 2)
-                            (expt (- c1.i c2.i) 2)))))
-          (cond ((> c.i 0)
-                 (new (complex (/ (sqrt (+ c.re len))
-                                  (sqrt 2))
-                               (/ (sqrt (+ (- c.re) len))
-                                  (sqrt 2)))))
-                ((< c.i 0)
-                 (new (complex (/ (sqrt (+ c.re len))
-                                  (sqrt 2))
-                               (/ (- (sqrt (+ (- c.re) len)))
-                                  (sqrt 2)))))
-                ((< c.re 0)
-                 (new (complex 0 (sqrt (abs c.re)))))
-                (t (new (complex (sqrt c.re))))))))
-    (defps-static complex infinityp
-      (lambda (c)
-        (or (= c.re (@ -number +positive_infinity+))
-            (= c.i (@ -number +positive_infinity+)))))
-    (defps-static complex zerop
-      (lambda (c)
-        (and (= c.re 0)
-             (= c.i 0))))
-    (defps-static complex realp
-      (lambda (c)
-        (= c.i 0)))))
+          (defpsmethod complex add (c)
+                       (new (complex (+ this.re c.re)
+                                     (+ this.i c.i))))
+          (defpsmethod complex sub (c)
+                       (new (complex (- this.re c.re)
+                                     (- this.i c.i))))
+          (defpsmethod complex mult (c)
+                       (new (complex (- (* this.re c.re)
+                                        (* this.i c.i))
+                                     (+ (* this.re c.i)
+                                        (* this.i c.re)))))
+          (defpsmethod complex div (c)
+                       (let ((denom (+ (* c.re c.re)
+                                       (* c.i c.i))))
+                         (cond ((= denom 0)
+                                (@ complex +infinity+))
+                               ((= denom (@ -number +positive_infinity+))
+                                (@ complex +zero+))
+                               (t (new (complex (/ (+ (* this.re c.re)
+                                                      (* this.i c.i))
+                                                   denom)
+                                                (/ (- (* this.i c.re)
+                                                      (* this.re c.i))
+                                                   denom)))))))
+          (defps-static complex dist
+            (lambda (c1 c2)
+              (sqrt (+ (expt (- c1.re c2.re) 2)
+                       (expt (- c1.i c2.i) 2)))))
+          (defps-static complex sqrt
+            (lambda (c)
+              (let ((len (sqrt (+ (expt (- c1.re c2.re) 2)
+                                  (expt (- c1.i c2.i) 2)))))
+                (cond ((> c.i 0)
+                       (new (complex (/ (sqrt (+ c.re len))
+                                        (sqrt 2))
+                                     (/ (sqrt (+ (- c.re) len))
+                                        (sqrt 2)))))
+                      ((< c.i 0)
+                       (new (complex (/ (sqrt (+ c.re len))
+                                        (sqrt 2))
+                                     (/ (- (sqrt (+ (- c.re) len)))
+                                        (sqrt 2)))))
+                      ((< c.re 0)
+                       (new (complex 0 (sqrt (abs c.re)))))
+                      (t (new (complex (sqrt c.re))))))))
+          (defps-static complex infinityp
+            (lambda (c)
+              (or (= c.re (@ -number +positive_infinity+))
+                  (= c.i (@ -number +positive_infinity+)))))
+          (defps-static complex zerop
+            (lambda (c)
+              (and (= c.re 0)
+                   (= c.i 0))))
+          (defps-static complex realp
+            (lambda (c)
+              (= c.i 0))))))
 
 (defun to-complex (tree)
        (cond ((numberp tree) `(new (complex ,tree)))
@@ -111,8 +114,11 @@
 
 (defmacro+ps c-prod (&rest args)
   (reduce #'(lambda (arg1 arg2)
-	      `((@ ,arg1 sub) ,arg2))
+	      `((@ ,arg1 mult) ,arg2))
           (to-complex args)))
 
-(setf (gethash #P"common/complex.js" *built-js-dic*)
-      (build-complex))
+(defmacro+ps c-quot (&rest args)
+  (reduce #'(lambda (arg1 arg2)
+	      `((@ ,arg1 div) ,arg2))
+          (to-complex args)))
+
